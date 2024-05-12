008e6a9c3fcb536c0f088f09557627429d942e72
Rizajin <jarkko.vaisanen@live.fi>
2024-05-12 20:12:23 +0300

    make it compile

---
+++ kernel.h
@@ -0,0 +1,3 @@
+#pragma once
+#include <vector>
+bool ExecuteCuda(const std::vector<int>& setA, const std::vector<int>& setB);
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <stdexcept>
-#include "kernel.cu"
+#include "kernel.h"
 
 int main()
 {
-    if (!ExecuteCuda()) {
+    if (!ExecuteCuda({1,2,3,4,5}, {10,20,30,40,50})) {
         throw new std::runtime_error("Failed with execute unit for CUDA");
     }
 
