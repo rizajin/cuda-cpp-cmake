6351f64218191075dd9cf1d128f22652e75a6ba3
Rizajin <jarkko.vaisanen@live.fi>
2024-05-12 20:12:23 +0300

    make it compile

--- kernel.cu
+++ kernel.cu
@@ -1,12 +1,19 @@
+#include "kernel.h"
+
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-__global__ void x()
+__global__ void addKernel(const int* a, const int* b, int* result)
 {
-
+    int i = threadIdx.x;
+    result[i] = a[i] + b[i];
 }
 
-bool ExecuteCuda()
+
+
+bool ExecuteCuda(const std::vector<int>& setA, const std::vector<int>& setB)
 {
-    return false;
+
+
+    return true;
 }
